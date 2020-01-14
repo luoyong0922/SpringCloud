178ca0c780d8cc36770e3f7b02283a1f44aad353
Roy Luo <1439183714@qq.com>
2020-01-14 16:07:20 +0800

    [新增] added config client

---
+++ eduManage/config-client/src/main/java/com/roy/springcloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,39 @@
+package com.roy.springcloud.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+    // 防止未找到相关属性 （如：${foo}）而导致项目启动失败
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setIgnoreUnresolvablePlaceholders(true);
+        return c;
+    }
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+
+    @Value("${args}")
+    String args;
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return args;
+    }
+}
